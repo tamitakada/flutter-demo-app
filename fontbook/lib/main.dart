a16a3636a440ad401d024f875d7607de5b5a888a
Tami Takada <tamitakada@gmail.com>
2022-10-18 17:20:12 -0400

    added key buttons & fonts list for home page

--- fontbook/lib/main.dart
+++ fontbook/lib/main.dart
@@ -1,115 +1,150 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:fontbook/mixins/fonts.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:fontbook/auth/pages/login.dart';
+import 'package:fontbook/widgets/font_cell.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
+        backgroundColor: Colors.white,
+        textTheme: TextTheme(
+          headline1: GoogleFonts.megrim(
+            fontSize: 36,
+            color: Colors.black
+          ),
+          headline3: GoogleFonts.zenMaruGothic(
+            fontSize: 16,
+            color: Colors.black
+          ),
+          bodyText1: GoogleFonts.zenMaruGothic(
+              fontSize: 18,
+              color: Colors.black
+          )
+        )
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const HomePage(title: 'FONTBOOK'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+class HomePage extends StatefulWidget {
+  const HomePage({super.key, required this.title});
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomePageState extends State<HomePage> with FontUtils {
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  String sampleText = "Ready, set, launch!";
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return FutureBuilder<List<String>>(
+      future: getAllFontNames(),
+      builder: (BuildContext context, AsyncSnapshot<List<String>> snapshot) {
+        if (snapshot.hasData) {
+          List<String> fonts = snapshot.data!;
+          return Scaffold(
+            backgroundColor: Theme.of(context).backgroundColor,
+            appBar: AppBar(
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              actions: [
+                IconButton(
+                  onPressed: () {
+                    // Navigator.of(context).push(
+                    //   CupertinoPageRoute(
+                    //     fullscreenDialog: true,
+                    //     builder: (context) => SearchPage(),
+                    //   ),
+                    // );
+                  },
+                  icon: const Icon(
+                    Icons.search_rounded,
+                    color: Colors.black,
+                    size: 28,
+                  ),
+                ),
+                IconButton(
+                    onPressed: () {
+                      Navigator.of(context).push(
+                        CupertinoPageRoute(
+                          fullscreenDialog: true,
+                          builder: (context) => LoginPage(),
+                        ),
+                      );
+                    },
+                    icon: const Icon(
+                      Icons.star_border_rounded,
+                      color: Colors.black,
+                      size: 30,
+                    )
+                )
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            body: ListView.builder(
+              itemCount: fonts.length + 1,
+              itemBuilder: (BuildContext context, int i) {
+                if (i == 0) {
+                  return Padding(
+                    padding: const EdgeInsets.fromLTRB(20, 10, 20, 20),
+                    child: Column(
+                      children: [
+                        Text(
+                          widget.title,
+                          style: Theme.of(context).textTheme.headline1,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 10),
+                          child: TextField(
+                            style: Theme.of(context).textTheme.bodyText1,
+                            decoration: InputDecoration(
+                              hintText: "Enter example sentence"
+                            ),
+                            onSubmitted: (text) {
+                              setState(() {
+                                if (text == null || text.trim().isEmpty) {
+                                  sampleText = "Ready, set, launch!";
+                                } else { sampleText = text; }
+                              });
+                            },
+                          ),
+                        )
+                      ],
+                    ),
+                  );
+                }
+                return FontCell(
+                  text: sampleText,
+                  fontName: fonts[i],
+                  favorited: false,
+                );
+              },
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+          );
+        } else { return Container(color: Colors.black); }
+      }
     );
   }
 }
